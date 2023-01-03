70a1792c247f0e0a99610779a23e442aa9b115d4
AliHussein <alihusseiinali@gmail.com>
2023-01-03 17:41:12 +0200

    org-files-update

--- index.php
+++ index.php
@@ -2,13 +2,9 @@
 <?php
 require_once('./layout/header.php');
 require_once('./logic/products.php');
+require_once('./logic/categories.php');
 
-$categories = [
-    ['id' => 1, 'name' => 'cat1', 'image' => 'img/cat-1.jpg'],
-    ['id' => 2, 'name' => 'cat2', 'image' => 'img/cat-2.jpg'],
-    ['id' => 3, 'name' => 'cat3', 'image' => 'img/cat-3.jpg'],
-    ['id' => 4, 'name' => 'cat4', 'image' => 'img/cat-4.jpg'],
-];
+$categories = getCategories();
 $products = getProducts();
 
 ?>
---
+++ logic/categories.php
@@ -0,0 +1,11 @@
+<?php
+function getCategories()
+{
+  $categories = [
+    ['id' => 1, 'name' => 'cat1', 'image' => 'img/cat-1.jpg'],
+    ['id' => 2, 'name' => 'cat2', 'image' => 'img/cat-2.jpg'],
+    ['id' => 3, 'name' => 'cat3', 'image' => 'img/cat-3.jpg'],
+    ['id' => 4, 'name' => 'cat4', 'image' => 'img/cat-4.jpg'],
+  ];
+  return $categories;
+}
