49c59897d389d13ed9ad9568d90da52631c68e5b
AliHussein <alihusseiinali@gmail.com>
2023-01-14 02:45:47 +0200

    php_task_3

---
+++ online-shop/decq.php
@@ -0,0 +1,13 @@
+<?php
+define('BASE_PATH', './');
+require_once('./logic/products.php');
+require_once('./logic/cart.php');
+$cart = getCart();
+if (isset($_GET['id'])) {
+    $product = getProductById($_GET['id']);
+    if ($product) {
+        decQun($product);
+    }
+}
+header('Location:cart.php');
+?>
---
+++ online-shop/incQu.php
@@ -0,0 +1,13 @@
+<?php
+define('BASE_PATH', './');
+require_once('./logic/products.php');
+require_once('./logic/cart.php');
+
+if (isset($_GET['id'])) {
+    $product = getProductById($_GET['id']);
+    if ($product) {
+        incQun($product);
+    }
+}
+header('Location:cart.php');
+?>
